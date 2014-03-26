4c2f2f1810460ff45fa50f70891b4fde3020dd8c
tinymins <tinymins@gmail.com>
2014-03-26 18:36:52 +0800

    fix url bug

--- wp-hatmore-localpic/wp-hatmore-localpic.class.php
+++ wp-hatmore-localpic/wp-hatmore-localpic.class.php
@@ -1,36 +1,37 @@
 <?php
 class hatmore{
-    
     public  $pid = '';
     public  $imgoldurl=array();
     public  $imgnewurl=array();
-
-
     public function upload(){
+        foreach ( $this->imgoldurl as $v ){
+            $pkwall = array('user-agent'=>'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0');
+            
+            $file_ext_name = '.png';
+            $matches = array();
+            if( preg_match('/^[^\'"]+(\.[a-z]{3,4})$/i', $v, $matches) ){ 
+                $file_ext_name = $matches[1];
+            }
+            
+            $response = wp_remote_get( $v, $pkwall );
+            $response_mime = wp_remote_retrieve_header( $response, 'content-type' );
+            
+            // $upload = wp_upload_bits(rawurldecode(basename($v)), '', wp_remote_retrieve_body( $response ) );
+            $upload = wp_upload_bits( date("YmdHis").mt_rand(1000,9999).$file_ext_name, null, wp_remote_retrieve_body( $response ) );
+            
+            $attachment = array(
+                'post_title'=> basename( $v ),
+                'post_mime_type' => $response_mime
+            );
+            $attach_id = wp_insert_attachment( $attachment, $upload['file'], $this->pid );
+            $attach_data = wp_generate_attachment_metadata( $attach_id, $v );
+            
+            wp_update_attachment_metadata( $attach_id, $attach_data );
+            # 如果没有缩略图则设置当前图片为文章缩略图。（文章第一张图片）
+            if(!has_post_thumbnail($this->pid)) { set_post_thumbnail($this->pid, $attach_id); }
         
-        foreach ($this->imgoldurl as $v){
-             $pkwall = array('user-agent'=>'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:17.0) Gecko/20100101 Firefox/17.0');
-             
-             if(!preg_match('/^([^\'"]+)(\.[a-z]{3,4})$\b/i',$v)){ 
-                 $v.='.png';
-             }
-             
-	     $get = wp_remote_get($v,$pkwall);
-	     $type = wp_remote_retrieve_header( $get, 'content-type' );
-        
-	     $mirror = wp_upload_bits(rawurldecode(basename($v)), '', wp_remote_retrieve_body( $get ) );
-	
-	     $attachment = array(
-		  'post_title'=> basename( $v ),
-		  'post_mime_type' => $type
-		);	
-	$attach_id = wp_insert_attachment( $attachment, $mirror['file'], $this->pid);
-	$attach_data = wp_generate_attachment_metadata( $attach_id, $v );
-	
-	wp_update_attachment_metadata( $attach_id, $attach_data );
-	set_post_thumbnail($this-pid, $attach_id);
-	
-        $this->imgnewurl[] = $mirror[url];
-    }}
+            $this->imgnewurl[] = $upload['url'];
+        }
+    }
 }
 ?>
--- wp-hatmore-localpic/wp-hatmore-localpic.php
+++ wp-hatmore-localpic/wp-hatmore-localpic.php
@@ -4,39 +4,42 @@ Plugin Name: wp-hatmore-localpic
 Plugin URI: http://hatmore.com
 Description:1: 自动检测文章中是否存在远程图片,如果存在，就下载到本地，并更正图片src,相同的图片只下载一次
 Version: 2.1
-Author: if
-Author URI: http://hatmore.com
+Author: tinymins
+Author URI: http://zhaiyiming.com
 */
 add_action('publish_post', 'newbody');
 
+error_reporting(E_ERROR | E_PARSE);
 function newbody($post_ID){
-    $reg= '/<img[^>]+src=[\'"]([^\'"]+)[\'"][^>]*>/i';   
-    $temp1 =array();
-    $temp2= array();
     remove_action('publish_post', 'newbody');
-    $body = get_post($post_ID);
-    preg_match_all($reg,$body->post_content,$temp1);
+    $post = get_post($post_ID);
+    $content = $post->post_content;
+    # 匹配图片标签的正则表达式
+    $sz_reg_img = '/<img[^>]+src=[\'"]([^\'"]+)[\'"][^>]*>/i';   
+    $matches_img_tag = array();
+    $matches_img_url = array();
+    preg_match_all( $sz_reg_img, $content, $matches_img_tag );
     
-    if(!empty($temp1)) $temp2= array_unique($temp1[1]);
-  
-    if(!empty($temp2)) {
-        
-        foreach($temp2 as $k=>$v){  
-            if (!strpos($v,$_SERVER['HTTP_HOST'])===false) unset($temp2[$k]);
+    if( !empty($matches_img_tag) ) { $matches_img_url = array_unique($matches_img_tag[1]); }
+    
+    if( !empty($matches_img_url) ) {
+        foreach( $matches_img_url as $k=>$v ) {
+            # 过滤掉本站的资源if( preg_match('/^[^\'"]+(\.[a-z]{3,4})$/i', $v, $matches) ){ 
+            if ( strpos($v,$_SERVER['HTTP_HOST'])!==false || !preg_match('/^[a-z]+:\/\/.*$/i', $v) ) { unset( $matches_img_url[$k] ); }
         }
     }
     
     require_once 'wp-hatmore-localpic.class.php';
     $img = new hatmore();
     $img->pid = $post_ID;
-    $img->imgoldurl = $temp2;
+    $img->imgoldurl = $matches_img_url;
     
-    if(!empty($img->imgoldurl)) {               
+    if(!empty($img->imgoldurl)) {
         $img->upload();
         $tag = array_combine($img->imgoldurl, $img->imgnewurl);
-        $newbody = strtr($body->post_content,$tag);
-        wp_update_post(array('ID' => $post_ID, 'post_content' => $newbody));//打印输出
-  }
-     add_action('publish_post', 'newbody');
+        $new_content = strtr( $content, $tag );
+        wp_update_post(array('ID' => $post_ID, 'post_content' => $new_content));//打印输出
+    }
+    add_action('publish_post', 'newbody');
 }
 ?>
